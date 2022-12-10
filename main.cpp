665b55ca21a345e6190ecc2da38cfe92af958943
Artem <334947@niuitmo.ru>
2022-12-10 21:11:47 +0300

    topological-sort

--- main.cpp
+++ main.cpp
@@ -2,67 +2,46 @@
 
 using namespace std;
 
-const int INFINITIVE = INT_MAX;
+//const int INFINITIVE = INT_MAX;
 const int EDGE_MIN_WEIGHT = -1000;
 
-int one_k_bfs(int n, int s, int t, int k, const vector<vector<pair<int, int>>> &g) {
-    vector<queue<int>> myQueue (k + 1);
-    myQueue[0].push(s);
-    vector<pair<int, int>> dist (n, make_pair(INFINITIVE, 0));
-    dist[s].first = 0;
-    vector<bool> used (n, false);
-    int index_nonEmpty_queue = 0;
-    int size_of_queue = 1;
-    while (size_of_queue > 0) {
-        while (myQueue[index_nonEmpty_queue % (k + 1)].empty()) {
-            index_nonEmpty_queue++;
-        }
-        int u = myQueue[index_nonEmpty_queue % (k + 1)].front();
-        myQueue[index_nonEmpty_queue % (k + 1)].pop();
-        size_of_queue--;
-        if (!used[u]) {
-            used[u] = true;
-            for (auto edge : g[u]) {
-                int v = edge.first;
-                int w = edge.second;
-                if (dist[u].first + dist[u].second * (EDGE_MIN_WEIGHT - 1) + w + (EDGE_MIN_WEIGHT - 1) < dist[v].first + dist[v].second * (EDGE_MIN_WEIGHT - 1)) {
-                    dist[v].first = dist[u].first + w;
-                    dist[v].second = dist[u].second + 1;
-                    myQueue[dist[v].first % (k + 1)].push(v);
-                    size_of_queue++;
-                }
-            }
+void dfs(int v, vector<bool> &used, vector<int> &ans, vector<vector<pair<int, int>>> &g) {
+    used[v] = true;
+    for (size_t i = 0; i < g[v].size(); i++) {
+        int to = g[v][i].first;
+        if (!used[to]) {
+            dfs(to, used, ans, g);
         }
     }
-    int res = dist[t].first + dist[t].second * (EDGE_MIN_WEIGHT - 1);
-    return res;
+    ans.push_back(v);
 }
 
 int main() {
     int n, m, s, t;
-    int k = -2*EDGE_MIN_WEIGHT+1;
     cin >> n >> m >> s >> t;
     s--;
     t--;
     vector<vector<pair<int, int>>> g(n);
+    vector<bool> used(n, false);
     for (int i = 0; i < m; i++) {
         int v, to, w;
         cin >> v >> to >> w;
         v--;
         to--;
-        w = w - EDGE_MIN_WEIGHT + 1;
+//        w = w - EDGE_MIN_WEIGHT + 1;
         g[v].emplace_back(to, w);
     }
-    int res = one_k_bfs(n, s, t, k, g);
+    vector<int> ans;
 
-    //TODO: stress-test with Ford-Bellman algorithm
-
-    if (res == INFINITIVE) {
-        cout << "Unreachable" << '\n';
-    } else {
-        cout << res << '\n';
+    for (int i = 0; i < n; i++) {
+        if (!used[i])
+            dfs(i, used, ans, g);
     }
-
+    reverse(ans.begin(), ans.end());
+    for (int i = 0; i < n; ++i) {
+        cout << ans[i]+1 << ' ';
+    }
+    cout << '\n';
     return 0;
 }
 
