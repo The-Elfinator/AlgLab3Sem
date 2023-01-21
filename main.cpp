b54920b69d925d26b6f288e5b4899a1e2a0b7af2
Artem <334947@niuitmo.ru>
2023-01-21 14:08:21 +0300

    task 3B input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
 
@@ -7,54 +6,45 @@ const int INF = INT_MAX;
 
 int main() {
     // input
-    int n;
-    cin >> n;
-    vector<pair<int, int>> g;
-    for (int i = 0; i < n; i++) {
-        int x, y;
-        cin >> x >> y;
-        g.emplace_back(x, y);
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> g(n + m);
+    for (int i = 0; i < m; i++) {
+        int x, y, z;
+        // x, y, z - which skyscrapers connected with magnet pillow
+        cin >> x >> y >> z;
+        x--;
+        y--;
+        z--;
+        int fictive_vertex = n+i;
+        g[x].push_back(fictive_vertex);
+        g[y].push_back(fictive_vertex);
+        g[z].push_back(fictive_vertex);
+        g[fictive_vertex].push_back(x);
+        g[fictive_vertex].push_back(y);
+        g[fictive_vertex].push_back(z);
     }
 
     /*<=======================================================================================>*/
     // solution
-    vector<bool> used (n, false);
-    vector<double> min_edge_from_vertex (n, INF);
-    vector<int> min_edge_end (n, -1);
-    min_edge_from_vertex[0] = 0;
-    double res = 0;
-    for (int i = 0; i < n; i++) {
-        int v = -1;
-        // find vertex with min out coming edge
-        for (int j = 0; j < n; j++) {
-            if (!used[j] && (v == -1 || min_edge_from_vertex[v] > min_edge_from_vertex[j])) {
-                v = j;
-            }
-        }
 
-        // try to improve the answer
-        used[v] = true;
-        if (min_edge_end[v] != -1) {
-            double dx = g[v].first - g[min_edge_end[v]].first;
-            double dy = g[v].second - g[min_edge_end[v]].second;
-            res += sqrt(dx*dx + dy*dy);
-        }
-
-        // renew min_edge_from_vertex vector if it's possible
-        for (int to = 0; to < n; to++) {
-            double edge_v_to_dx = g[v].first - g[to].first;
-            double edge_v_to_dy = g[v].second - g[to].second;
-            double edge_v_to_weight = sqrt(edge_v_to_dx * edge_v_to_dx + edge_v_to_dy * edge_v_to_dy);
-            if (edge_v_to_weight < min_edge_from_vertex[to]) {
-                min_edge_from_vertex[to] = edge_v_to_weight;
-                min_edge_end[to] = v;
-            }
-        }
-    }
     /*<=======================================================================================>*/
 
     // output
-    cout << res << '\n';
+    for (int i = 0; i < n; i++) {
+        cout << "Skyscraper " << i+1 << " connected with fictive vertex: ";
+        for (int v : g[i]) {
+            cout << v-n+1 << ' ';
+        }
+        cout << '\n';
+    }
+    for (int i = n; i < n+m; i++) {
+        cout << "Fictive vertex " << i-n+1 << " connected with skyscrapers: ";
+        for (int u : g[i]) {
+            cout << u+1 << ' ';
+        }
+        cout << '\n';
+    }
     return 0;
 }
 
