1e23794048449b8d7b6291490e2eec3bb25a64b1
Artem <334947@niuitmo.ru>
2023-01-23 15:05:48 +0300

    task 5A success

--- main.cpp
+++ main.cpp
@@ -2,97 +2,65 @@
 
 using namespace std;
 
-vector<vector<int>> g;
-vector<bool> used;
-int timer;
-vector<int> tin;
-vector<int> first_up;
-set<int> points;
-
-void dfs(int v, int pred = -1) {
-    used[v] = true;
-    tin[v] = timer;
-    first_up[v] = timer;
-    timer++;
-    int child = 0;
-    for (int i = 0; i < g[v].size(); i++) {
-        int to = g[v][i];
-        if (to == pred) {
-            continue;
-        }
-        if (used[to]) {
-            first_up[v] = min(first_up[v], tin[to]);
-        } else {
-            dfs(to, v);
-            first_up[v] = min(first_up[v], first_up[to]);
-            if (first_up[to] >= tin[v] && pred != -1) {
-                points.insert(v);
-            }
-            child++;
-        }
-    }
-    if (pred == -1 && child > 1) {
-        points.insert(v);
-    }
-
-}
+const int INF = INT_MAX;
 
 int main() {
     // input
-    int n, m;
-    cin >> n >> m;
-    g.assign(n + m, vector<int>());
-    used.assign(n+m, false);
-    timer = 0;
-    tin.assign(n + m, 0);
-    first_up.assign(n + m, 0);
-    for (int i = 0; i < m; i++) {
-        int x, y, z;
-        // x, y, z - which skyscrapers connected with magnet pillow
-        cin >> x >> y >> z;
+    int n, m, k;
+    cin >> n >> k;
+    vector<int> exits;
+    for (int i = 0; i < k; i++) {
+        int x;
+        cin >> x;
         x--;
-        y--;
-        z--;
-        int fictive_vertex = n+i;
-        g[x].push_back(fictive_vertex);
-        g[y].push_back(fictive_vertex);
-        g[z].push_back(fictive_vertex);
-        g[fictive_vertex].push_back(x);
-        g[fictive_vertex].push_back(y);
-        g[fictive_vertex].push_back(z);
+        exits.push_back(x);
+    }
+    cin >> m;
+    vector<vector<int>> g(n, vector<int>());
+    for (int i = 0; i < m; i++) {
+        int v, u;
+        cin >> v >> u;
+        v--;
+        u--;
+        g[v].push_back(u);
+        g[u].push_back(v);
     }
 
     /*<=======================================================================================>*/
     // solution
-    // Найти точки сочленения в полученном графе, которые являются фиктивными вершинами
-    dfs(0);
-    vector<int> points_fictive;
-    for (int v : points) {
-        if (v >= n) {
-            points_fictive.push_back(v - n + 1);
+    queue<int> my_queue;
+    vector<int> dist(n, INF);
+    vector<int> nearest_exit(n, -1);
+    for (int e : exits) {
+        my_queue.push(e);
+        dist[e] = 0;
+        nearest_exit[e] = e;
+    }
+    while (!my_queue.empty()) {
+        int v = my_queue.front();
+        my_queue.pop();
+        for (int to : g[v]) {
+            if (dist[to] > dist[v] + 1) {
+                dist[to] = dist[v] + 1;
+                nearest_exit[to] = nearest_exit[v];
+                my_queue.push(to);
+            } else if (dist[to] == dist[v] + 1) {
+                nearest_exit[to] = min(nearest_exit[to], nearest_exit[v]);
+                my_queue.push(to);
+            }
         }
     }
     /*<=======================================================================================>*/
 
     // output
-//    for (int i = 0; i < n; i++) {
-//        cout << "Skyscraper " << i+1 << " connected with fictive vertex: ";
-//        for (int v : g[i]) {
-//            cout << v-n+1 << ' ';
-//        }
-//        cout << '\n';
-//    }
-//    for (int i = n; i < n+m; i++) {
-//        cout << "Fictive vertex " << i-n+1 << " connected with skyscrapers: ";
-//        for (int u : g[i]) {
-//            cout << u+1 << ' ';
-//        }
-//        cout << '\n';
-//    }
-    cout << points_fictive.size() << '\n';
-    for (int v : points_fictive) {
-        cout << v << '\n';
+    for (int x : dist) {
+        cout << x << ' ';
     }
+    cout << '\n';
+    for (int v : nearest_exit) {
+        cout << v + 1 << ' ';
+    }
+    cout << '\n';
     return 0;
 }
 
